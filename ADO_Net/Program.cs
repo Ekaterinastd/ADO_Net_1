65aee8649fe1c39f9c6effeda48d913a67ee3544
Ekaterinastd <ekaterina.g2015@mail.ru>
2020-05-28 22:08:56 +0400

    Настроено соединение с ssms и обработка запросов select

---
+++ ADO_Net/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Net.Cache;
+
+namespace ADO_Net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            var sqlExpression = "select ContactName, Country from dbo.Customers where Country in ('USA', 'Canada') order by ContactName";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                Console.WriteLine("Connection is open");
+                var command = new SqlCommand(sqlExpression, connection);
+
+                var result1 = command.ExecuteNonQuery();
+
+                var result = command.ExecuteReader();
+                var i = 1;
+                Console.WriteLine("Request: select ContactName, Country from dbo.Customers where Country in ('USA', 'Canada') order by ContactName\n");
+                Console.WriteLine($"\tContactName \tCountry\n");
+                
+                while (result.Read())
+                {
+                    Console.WriteLine(string.Format("{0,-3} {1,-20} {2}", i, result[0], result[1]));
+                    i++;
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Connection is close");
+            Console.Read();
+        }
+    }
+}
