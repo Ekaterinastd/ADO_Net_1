43538e4dab5f2d3ca496a5893c7ac673154e27b9
Ekaterinastd <ekaterina.g2015@mail.ru>
2020-05-31 17:33:46 +0400

    Удалено неиспользуемое

--- AdoNetWInForms2/Form1.cs
+++ AdoNetWInForms2/Form1.cs
@@ -1,13 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace AdoNetWInForms2
@@ -40,27 +34,6 @@ namespace AdoNetWInForms2
         {
             using(var connection = new SqlConnection(connectionString))
             {
-            //    connection.Open();
-            //    var adapter = new SqlDataAdapter(request, connection);
-            //    var commandBuilder = new SqlCommandBuilder(adapter);
-            //    adapter.InsertCommand.CommandType = CommandType.StoredProcedure;
-            //    adapter.InsertCommand.Parameters.Add(new SqlParameter("@lastName", SqlDbType.NVarChar, 50, "LastName"));
-            //    adapter.InsertCommand.Parameters.Add(new SqlParameter("@firstName", SqlDbType.NVarChar, 50, "FirstName"));
-            //    var outputParam = adapter.InsertCommand.Parameters.Add("@Id", SqlDbType.Int, 0, "Id");
-            //    outputParam.Direction = ParameterDirection.Output;
-
-            //    ds = new DataSet();
-            //    adapter.Fill(ds);
-            //    var dt = ds.Tables[0];
-            //    var newRow = dt.NewRow();
-            //    newRow["LastName"] = "LastName1";
-            //    newRow["FirstName"] = "FirstName";
-            //    newRow["BirthDate"] = 30 / 05 / 2020;
-            //    dt.Rows.Add(newRow);
-
-            //    adapter.Update(ds);
-            //    ds.AcceptChanges();
-
                 var row = ds.Tables[0].NewRow();
                 ds.Tables[0].Rows.Add(row);
             }
@@ -88,7 +61,9 @@ namespace AdoNetWInForms2
                 adapter.InsertCommand.Parameters.Add(new SqlParameter("@birthDate", SqlDbType.DateTime,10, "BirthDate"));
                 var outputParam = adapter.InsertCommand.Parameters.Add("@Id", SqlDbType.Int, 0, "EmployeeID");
                 outputParam.Direction = ParameterDirection.Output;
+                var c = ds.Tables.Count;
                 adapter.Update(ds);
+                //var foreignKey= new ForeignKeyConstraint(ds.Tables)
             }
         }
     }
